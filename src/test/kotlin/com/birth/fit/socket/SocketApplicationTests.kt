9984409cd63b7808f706ed65645ea9ae5f4cb39d
Hanif <marbling1293@naver.com>
2021-04-23 08:59:03 +0900

    [ADD] Project Init

---
+++ src/test/kotlin/com/birth/fit/socket/SocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.birth.fit.socket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
