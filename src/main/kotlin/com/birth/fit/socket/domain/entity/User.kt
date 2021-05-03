f93a803bae8b30005999a53ecfdbf5fa310fe11a
Hanif <marbling1293@naver.com>
2021-05-03 11:52:54 +0900

    [ADD] user entity

--- src/main/kotlin/com/birth/fit/socket/domain/entity/User.kt
+++ src/main/kotlin/com/birth/fit/socket/domain/entity/User.kt
@@ -12,4 +12,14 @@ class User(
     @Id
     @Column(name = "email")
     internal val email: String,
+
+
+    @Column(name = "user_id")
+    internal var userId: String,
+
+    @Column(name = "password")
+    internal var password: String,
+
+    @Column(name = "image")
+    internal var image: String? = null
 )
