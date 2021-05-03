a1b24268c2f2b1c5ad0b8fa2d247aafb09a3299c
Hanif <marbling1293@naver.com>
2021-05-03 11:54:19 +0900

    [ADD] user repository

--- src/main/kotlin/com/birth/fit/socket/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/birth/fit/socket/domain/repository/UserRepository.kt
@@ -3,6 +3,9 @@ package com.birth.fit.socket.domain.repository
 import com.birth.fit.socket.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface UserRepository: JpaRepository<User, String>
+interface UserRepository: JpaRepository<User, String> {
+    fun findByEmail(email: String?): Optional<User?>
+}
