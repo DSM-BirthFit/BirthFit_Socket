72aee56e03ab365e0e086ad688e0afc3b776e800
Hanif <marbling1293@naver.com>
2021-05-07 12:43:44 +0900

    [FIX] socket.io server position

--- src/main/kotlin/com/birth/fit/socket/config/SocketConfig.kt
+++ src/main/kotlin/com/birth/fit/socket/config/SocketConfig.kt
@@ -1,16 +1,18 @@
 package com.birth.fit.socket.config
 
 import com.corundumstudio.socketio.SocketIOServer
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
 import javax.annotation.PreDestroy
 
 @Configuration
 class SocketConfig(
     @Value("\${server.socket.port}")
     private val port: Int,
-    private var server: SocketIOServer
+    @Autowired @Lazy private var server: SocketIOServer
 ) {
 
     @Bean
