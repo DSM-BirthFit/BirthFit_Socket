39595cc7d8df0167f78f42ea2f14acee7c9a5c14
Hanif <marbling1293@naver.com>
2021-05-07 13:17:45 +0900

    [ADD] cors filter

---
+++ src/main/kotlin/com/birth/fit/socket/config/CorsFilter.kt
@@ -0,0 +1,25 @@
+package com.birth.fit.socket.config
+
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class CorsFilter : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        response.addHeader("Access-Control-Allow-Origin", "*")
+        response.addHeader("Access-Control-Allow-Methods", "*")
+        response.addHeader("Access-Control-Allow-Headers", "*")
+        response.addHeader("Access-Control-Expose-Headers", "*")
+        response.addHeader("Access-Control-Allow-Credentials", "true")
+        response.addIntHeader("Access-Control-Max-Age", 10)
+        filterChain.doFilter(request, response)
+    }
+}
